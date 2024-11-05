376c5fc7d8dca4292cf7812c37cb2bfa72b8abcd
Mohammad NIAEI <m.shemuni@gmail.com>
2024-11-05 08:38:53 +0300

    Version increased

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myraf',
-    version='3.0',
+    version='3.1',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     py_modules=['main'],
